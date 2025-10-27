78e7c4646d7c2b4dc9ce9531caf8a5b29e45ea85
wesfff326 <wesfff@gmail.com>
2025-10-27 20:45:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -82,3 +82,4 @@ app.get("/setup", async (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
